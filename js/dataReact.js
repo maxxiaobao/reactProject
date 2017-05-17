c96e4fa1d5e5c917193d117ce0f01c0ec0f74196
xiulima <xiulima@FuzhuangLis-MacBook-Air.local>
2017-05-17 19:28:05 +0800

    learn react

---
+++ js/dataReact.js
@@ -0,0 +1,174 @@
+
+var getData = function(){
+		var dataJson = [
+			{
+				name: "shandong", 
+				lastInput: "2016-04-21",
+				forms: 324456,
+				voter: 13425,
+				update: "2017-03-22",
+				level: "levelone",
+				data:[
+					{
+						name: "jian",
+						lastInput: "2016-04-21",
+						forms: 324456,
+						voter: 13425,
+						update: "2017-03-22",
+						level: "leveltwo",
+						data:[
+							{
+								name: "lixia",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							},
+							{
+								name: "shizhong",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							},
+							{
+								name: "tianqiao",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							},
+							{
+								name: "huaiyin",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							}
+						]
+					},
+					{
+						name: "qingnao",
+						lastInput: "2016-04-21",
+						forms: 324456,
+						voter: 13425,
+						update: "2017-03-22",
+						level: "leveltwo",
+						data:[
+
+						]
+					},
+					{
+						name: "yantai",
+						lastInput: "2016-04-21",
+						forms: 324456,
+						voter: 13425,
+						update: "2017-03-22",
+						level: "leveltwo",
+						data:[
+
+						]
+					},
+					{
+						name: "weihai",
+						lastInput: "2016-04-21",
+						forms: 324456,
+						voter: 13425,
+						update: "2017-03-22",
+						level: "leveltwo",
+						data:[
+
+						]
+					},
+					{
+						name: "heze",
+						lastInput: "2016-04-21",
+						forms: 324456,
+						voter: 13425,
+						update: "2017-03-22",
+						level: "leveltwo",
+						data:[
+							{
+								name: "caoxian",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							}
+						]
+					}
+				]
+			},
+			{
+				name: "sichuan",
+				lastInput: "2016-04-21",
+				forms: 324456,
+				voter: 13425,
+				update: "2017-03-22",
+				level: "levelone",
+				data:[
+					{
+						name: "chengdu",
+						lastInput: "2016-04-21",
+						forms: 324456,
+						voter: 13425,
+						update: "2017-03-22",
+						level: "leveltwo",
+						data:[
+
+						]
+					}
+				]
+			},
+			{
+				name: "zhejiang",
+				lastInput: "2016-04-21",
+				forms: 324456,
+				voter: 13425,
+				update: "2017-03-22",
+				level: "levelone",
+				data:[
+					{
+						name: "hangzhou",
+						lastInput: "2016-04-21",
+						forms: 324456,
+						voter: 13425,
+						update: "2017-03-22",
+						level: "leveltwo",
+						data:[
+							{
+								name: "xihu",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							},
+							{
+								name: "shangcheng",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							},
+							{
+								name: "xiacheng",
+								lastInput: "2016-04-21",
+								forms: 324456,
+								voter: 13425,
+								update: "2017-03-22",
+								level: "levelthree"
+							}
+						]
+					}
+				]
+			}
+		]
+		return dataJson;
+	}
